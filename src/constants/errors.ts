6defd7af10c94d91ea9a76e645c452a76fd4a798
Kirill <qtrixnet@yandex.ru>
2025-02-01 00:23:58 +0500

    fix: move comments

--- src/constants/errors.ts
+++ src/constants/errors.ts
@@ -1,12 +1,14 @@
-/* eslint-disable no-unused-vars */
 export enum ErrorCode {
+  /* eslint-disable no-unused-vars */
   BAD_REQUEST = 400,
   NOT_FOUND = 404,
   INTERNAL_SERVER_ERROR = 500
+  /* eslint-enable no-unused-vars */
 }
 
 export enum ErrorMessage {
+  /* eslint-disable no-unused-vars */
   VALIDATION_ERROR = 'Ошибка валидации данных',
   INVALID_ID = 'Некорректный ID'
+  /* eslint-enable no-unused-vars */
 }
-/* eslint-enable no-unused-vars */
